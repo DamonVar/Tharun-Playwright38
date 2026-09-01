4a23be369a4c50e022219b865c2401bb758e03da
DamonVar <damonvar191@gmail.com>
2026-09-01 23:47:53 +0530

    commiting code for github actions and enhanced reporting

--- playwright.config.js
+++ playwright.config.js
@@ -15,23 +15,24 @@ require('dotenv').config()
 export default defineConfig({
   testDir: './demos',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: process.env.CI ? false : true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['junit', { outputFile: 'junit-results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless : false,
+    headless: process.env.CI ? true : false,
     /* Base URL to use in actions like `await page.goto('')`. */
-     baseURL: 'http://localhost:3000',
-
+    baseURL: 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: process.env.CI ? 'on-failure' : 'on-first-retry',
+    /* Take screenshot on failure in CI */
+    screenshot: process.env.CI ? 'only-on-failure' : 'off',
   },
 
   /* Configure projects for major browsers */
